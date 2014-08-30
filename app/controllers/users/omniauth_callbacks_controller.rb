fc07e47c6fb960d94276656a10aab0815e8424ca
Neson <neson@dex.tw>
2014-08-31 00:02:05 +0800

    fix login status cookie problem

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.from_uapp(request.env["omniauth.auth"])
     sign_in @user
     set_flash_message(:notice, :success, :kind => "UAPP") if is_navigational_format?
-    cookies[:user_logined] = { value: true, domain: Setting.uapp_url.gsub(/https?:\/\//, '').gsub(/\//, '').gsub(/^[^\.]*/, '') }
+    # cookies[:user_logined] = { value: true, domain: Setting.uapp_url.gsub(/https?:\/\//, '').gsub(/\//, '').gsub(/^[^\.]*/, '') }
     cookies[:user_logined] = { value: true, domain: '.' + Setting.uapp_url.gsub(/https?:\/\//, '').gsub(/\//, '') }
     redirect_to session[:login_redirect_path] || root_path
   end
